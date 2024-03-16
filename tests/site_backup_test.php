d214f155fd3f54557f46e807e34e1755e1f5a47c
Marina Glancy <marina.glancy@gmail.com>
2024-03-16 13:03:30 +0000

    Remove another weird test

--- tests/site_backup_test.php
+++ tests/site_backup_test.php
@@ -76,34 +76,6 @@ class site_backup_test extends \advanced_testcase {
         $this->assertTrue(array_key_exists('config', $tables));
     }
 
-    public function test_export_table() {
-        $this->resetAfterTest();
-        $sitebackup = $this->create_site_backup();
-        $tableobj = dbtable::create_from_actual_db('tool_vault_config', $sitebackup->get_db_structure());
-
-        // We will use table tool_vault_config as a temp table.
-        api::store_config('n0', 'value');
-        api::store_config('n1', null);
-        api::store_config('n2', '');
-        api::store_config('n3', 'null');
-        api::store_config('n4', 'NULL');
-        api::store_config('n5', 'value "with" quotes');
-        api::store_config('n6', "value\nwith\nnewlines");
-
-        $dir = tempfiles::make_temp_dir('test-dbstruct-');
-        $sitebackup->export_table_data($tableobj, $dir);
-
-        $data = json_decode(file_get_contents($dir.DIRECTORY_SEPARATOR.'tool_vault_config.0.json'), true);
-        $this->assertEquals(['name', 'n0', 'n1', 'n2', 'n3', 'n4', 'n5', 'n6'], array_column($data, 1));
-        $this->assertEquals(['value', 'value', null, '', 'null', 'NULL', 'value "with" quotes', "value\nwith\nnewlines"],
-            array_column($data, 2));
-
-        // Cleanup (there is one temp file that will be removed and debugging is called).
-        tempfiles::remove_temp_dir($dir);
-        tempfiles::cleanup();
-        $this->assertDebuggingCalled();
-    }
-
     public function test_export_db() {
         if (!PHPUNIT_LONGTEST) {
             $this->markTestSkipped('PHPUNIT_LONGTEST is not defined');
