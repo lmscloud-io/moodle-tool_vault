dca9dea8d5d3b585695ac7c00cd3457338359eeb
Marina Glancy <marina.glancy@gmail.com>
2024-03-16 19:22:24 +0000

    v1.0

--- version.php
+++ version.php
@@ -26,8 +26,8 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component    = 'tool_vault';
-$plugin->release      = '0.2.0';
-$plugin->version      = 2024021000;
+$plugin->release      = '1.0';
+$plugin->version      = 2024031600;
 $plugin->requires     = 2020061500;
-$plugin->supported    = [39, 403];
-$plugin->maturity     = MATURITY_ALPHA;
+$plugin->supported    = [39, 404];
+$plugin->maturity     = MATURITY_STABLE;
