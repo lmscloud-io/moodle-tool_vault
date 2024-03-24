5e83779d18f0858c5d9ad623a9c5171c0c0184aa
Marina Glancy <marina.glancy@gmail.com>
2024-03-24 19:15:00 +0000

    v1.1

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+
+All notable changes to the tool_vault plugin will be documented in this file.
+
+## [1.1] - 2024-03-24
+
+### Added
+- Privacy provider notification that the site is backed up to the external location
+- Syntax validation for submitted API key
+
+## [1.0] - 2024-03-16
+Initial release
--- version.php
+++ version.php
@@ -26,8 +26,8 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component    = 'tool_vault';
-$plugin->release      = '1.0';
-$plugin->version      = 2024031600;
+$plugin->release      = '1.1';
+$plugin->version      = 2024032400;
 $plugin->requires     = 2020061500;
 $plugin->supported    = [39, 404];
 $plugin->maturity     = MATURITY_STABLE;
