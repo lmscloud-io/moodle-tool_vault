29a4dda8c2ff44f97e41fd16279bc8b826a465ee
Marina Glancy <marina.glancy@gmail.com>
2024-11-23 18:05:35 +0000

    Upgrade 3.6 - coding style, etc

--- classes/local/restoreactions/upgrade_36/tool_dataprivacy.php
+++ classes/local/restoreactions/upgrade_36/tool_dataprivacy.php
@@ -14,6 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+// phpcs:ignoreFile
+// Mdlcode-disable incorrect-package-name.
+
 /**
  * tool_dataprivacy plugin upgrade code
  *
